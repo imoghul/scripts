e57cb83e0b9d0caf8d3dd3a02be76204e67950ec
imoghul <moghul02@gmail.com>
2021-09-13 08:02:09 -0400

    added aliasess

--- aliases.bash
+++ aliases.bash
@@ -1,3 +1,7 @@
 alias rmPdfs="python3 ~/scripts/pdfRemover.py"
 alias style="~/scripts/style.sh"
 alias run="~/scripts/run.py"
+alias c="clear && pwd && echo && ls"
+alias ll='ls -alF'
+alias l='ls -CF'
+alias la="ls -a"
