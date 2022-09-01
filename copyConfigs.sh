ca1eb4c7f0d014b6ccf1a9c387b13ba4862cc7c1
immoghul <moghul02@gmail.com>
2022-09-01 14:18:57 -0400

    upgrade to i3blocks

--- copyConfigs.sh
+++ copyConfigs.sh
@@ -1,5 +1,5 @@
 cp ~/.config/i3/config configs
-cp /etc/i3status.conf configs
+cp ~/.config/i3/i3blocks.conf configs
 cp ~/.Xresources configs
 cp ~/.urxvt/ext/* configs/urxvt
 cp ~/.Xresources configs
--- copyOverConfigs.sh
+++ copyOverConfigs.sh
@@ -1,3 +1,3 @@
 cp configs/config ~/.config/i3/config
 cp configs/.Xresources ~/.Xresources
-sudo cp configs/i3status.conf /etc/i3status.conf
+sudo cp configs/i3blocks.conf ~/.config/i3
