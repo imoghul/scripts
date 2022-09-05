70c93d0f9f20f6f2d77b782d428f63efa9ef7735
immoghul <moghul02@gmail.com>
2022-09-05 14:39:18 -0400

    added rofi config and changed wallpaper

--- copyConfigs.sh
+++ copyConfigs.sh
@@ -3,3 +3,4 @@ cp ~/.config/i3/i3blocks.conf configs
 cp ~/.Xresources configs
 cp ~/.urxvt/ext/* configs/urxvt
 cp ~/.Xresources configs
+cp ~/.config/rofi/config.rasi configs
--- copyOverConfigs.sh
+++ copyOverConfigs.sh
@@ -1,3 +1,4 @@
 cp configs/config ~/.config/i3/config
 cp configs/.Xresources ~/.Xresources
 sudo cp configs/i3blocks.conf ~/.config/i3
+sudo cp configs/config.rasi ~/.config/rofi
