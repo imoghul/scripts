abb6ac77447282f3163acab3b7bea98a734a2a05
immoghul <moghul02@gmail.com>
2022-09-05 14:43:49 -0400

    fixed active window opacity

--- copyConfigs.sh
+++ copyConfigs.sh
@@ -1,6 +1,8 @@
+cd ~/scripts
 cp ~/.config/i3/config configs
 cp ~/.config/i3/i3blocks.conf configs
 cp ~/.Xresources configs
 cp ~/.urxvt/ext/* configs/urxvt
 cp ~/.Xresources configs
 cp ~/.config/rofi/config.rasi configs
+cp ~/.config/compton/compton.conf configs
--- copyOverConfigs.sh
+++ copyOverConfigs.sh
@@ -1,4 +1,6 @@
+cd ~/scripts
 cp configs/config ~/.config/i3/config
 cp configs/.Xresources ~/.Xresources
 sudo cp configs/i3blocks.conf ~/.config/i3
 sudo cp configs/config.rasi ~/.config/rofi
+sudo cp configs/compton.conf ~/.config/compton
