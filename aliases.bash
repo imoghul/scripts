59aeaec8a0fda8e485a9c23748d54c6a9d5bda1b
imoghul <moghul02@gmail.com>
2023-09-27 10:55:42 -0400

    made aliases.bash more platform compatible

--- aliases.bash
+++ aliases.bash
@@ -7,22 +7,23 @@ alias l='ls -CF'
 alias la="ls -a"
 alias ncsussh="ssh immoghul@remote.eos.ncsu.edu"
 alias rpissh="ssh pi@ibrahimpi.local"
+alias arcssh="ssh -i ~/ssh_keys/506_arc_key immoghul@arc.csc.ncsu.edu"
 alias grendel="ssh -Y immoghul@grendel.ece.ncsu.edu"
 alias wheather="resize -s 42 125 && curl wttr.in"
 alias recompileURxvt="xrdb ~/.Xresources"
 alias backlight="~/scripts/kbdbacklight.sh"
-alias brightness="xrandr --output $(xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1) --brightness"
-case "$(which zsh)" in
-  *bash*)   PS1="\u \w %  " ;;
-  *zsh*)  PROMPT="%B%F{256}%n%f%b %U%1~%u %# " ;;
-  *)        echo "unknown: $0" ;;
-esac
-if [[ "$TERM" = xterm ]] ; then
-    if [[ "$OSTYPE" = darwin* ]] ; then
-        PS1='%2~$ '
-    fi
-    if [[ "$OSTYPE" = linux* ]] ; then
-        PS1='\u$ '
-    fi
-fi
+#alias brightness="xrandr --output $(xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1) --brightness"
+#case "$(which zsh)" in
+#  *bash*)   PS1="\u \w %  " ;;
+#  *zsh*)  PROMPT="%B%F{256}%n%f%b %U%1~%u %# " ;;
+#  *)        echo "unknown: $0" ;;
+#esac
+#if [[ "$TERM" = xterm ]] ; then
+#    if [[ "$OSTYPE" = darwin* ]] ; then
+#        PS1='%2~$ '
+#    fi
+#    if [[ "$OSTYPE" = linux* ]] ; then
+#        PS1='\u$ '
+#    fi
+#fi
 alias batteryLevel="cat /sys/class/power_supply/BAT0/capacity"
